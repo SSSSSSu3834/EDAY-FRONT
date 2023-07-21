dd7674d8f17883d604a8b1cdf924e2303471632b
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-07-22 04:28:37 +0900

    ✨ : InfoContent + Tip + Img 생성

---
+++ src/components/_common/Img.js
@@ -0,0 +1,11 @@
+import imageSrc from '../../assets/img.svg';
+
+const Img = () => {
+    return (
+        <div className='Img'>
+            <img src={imageSrc} />
+        </div>
+    );
+};
+
+export default Img;
