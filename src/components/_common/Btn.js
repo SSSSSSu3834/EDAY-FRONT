71ef9ce1b55720bb7d029738bf6f7b83f871fdb5
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-07-21 19:47:39 +0900

    ✨ : 공용 Btn 생성

--- src/components/_common/Btn.js
+++ src/components/_common/Btn.js
@@ -1,1 +1,21 @@
-const Btn = () => {};
+const Btn = ({ text, type, onClick }) => {
+    const btnType = ['deepGreen', 'lightGreen'].includes(type)
+        ? type
+        : 'default';
+    //회색 버튼이 default type 안 넣으면 자동 회색 버튼.ㅠ
+
+    return (
+        <button
+            className={['Btn', `Btn_${btnType}`].join(' ')}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
+};
+
+Btn.defaultProps = {
+    type: 'default',
+};
+
+export default Btn;
