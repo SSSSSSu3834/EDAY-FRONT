0e80c3f16dbd6ded74a9802645da34116db95f14
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-07-25 20:54:40 +0900

    ✨ : 스타일 코드 수정

--- src/components/_common/Btn.jsx
+++ src/components/_common/Btn.jsx
@@ -2,7 +2,7 @@ const Btn = ({ text, type, onClick }) => {
     const btnType = ['deepGreen', 'lightGreen'].includes(type)
         ? type
         : 'default';
-    //회색 버튼이 default type 안 넣으면 자동 회색 버튼.ㅠ
+    //회색 버튼이 default type 안 넣으면 자동 회색 버튼.
 
     return (
         <button
