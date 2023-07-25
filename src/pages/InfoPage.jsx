db7c76fa4d5f7e3d15d40141d2e606cc9b71ab81
서진 <dltjwls0529@gmail.com>
2023-07-25 20:53:52 +0900

    ✨ : 추가 정보 페이지 조건부 렌더링, 컴포넌트 생성

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import BodyBox from '../components/infopage/BodyBox';
+import { useParams } from 'react-router-dom';
+
+import Info1 from '../components/infopage/Info1';
+import Info2 from '../components/infopage/Info2';
+import Info3 from '../components/infopage/Info3';
+import Info4 from '../components/infopage/Info4';
+import Info5 from '../components/infopage/Info5';
+import Info6 from '../components/infopage/Info6';
+import Info7 from '../components/infopage/Info7';
 
 const InfoPage = () => {
+    const { dDay } = useParams();
     return (
-        <div className='InfoPage'>
-            <h1>InfoPage</h1>
-            <div className='box'>
-                <BodyBox />
-            </div>
-        </div>
+        <>
+            {dDay === '1' ? (
+                <Info1 />
+            ) : dDay === '2' ? (
+                <Info2 />
+            ) : dDay === '3' ? (
+                <Info3 />
+            ) : dDay === '4' ? (
+                <Info4 />
+            ) : dDay === '5' ? (
+                <Info5 />
+            ) : dDay === '6' ? (
+                <Info6 />
+            ) : dDay === '7' ? (
+                <Info7 />
+            ) : null}
+        </>
     );
 };
 
