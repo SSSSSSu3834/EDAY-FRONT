3f63e5a258cb02ba1cbe0bb2f5ab011023f6a457
ybchoi5262 <ybchoi5262@ewhain.net>
2023-07-28 14:24:14 +0900

    ✨ : AnswerPage 구현

--- src/components/quizpage/Header.jsx
+++ src/components/quizpage/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import XBtn from '../_common/XBtn';
 import Dday from '../_common/Dday';
@@ -6,6 +6,17 @@ import GreenBorder from '../_common/GreenBorder';
 import GreenBorderBox from '../_common/GreenBorderBox';
 
 const Header = ({ num, text }) => {
+    const greenBorderBoxRef = useRef(null);
+    const greenBorderBoxHeight = useRef(0);
+
+    useEffect(() => {
+        if (greenBorderBoxRef.current) {
+            greenBorderBoxHeight.current =
+                greenBorderBoxRef.current.clientHeight;
+            console.log('GreenBorderBox Height:', greenBorderBoxHeight.current);
+        }
+    });
+
     return (
         <>
             <Top>
@@ -14,8 +25,8 @@ const Header = ({ num, text }) => {
                     <XBtn />
                 </XBtnContainer>
             </Top>
-            <GreenBorder text='추가정보제목위치' />
-            <BoxWrapper>
+            <GreenBorder text='추가 정보 제목 위치' />
+            <BoxWrapper ref={greenBorderBoxRef}>
                 <GreenBorderBox>
                     퀴즈내용 어쩌고 가나다라마바ㄴㄴㅁㄻ니ㅓㄹ 아니아
                     거라호미ㅏㅓㅣ
