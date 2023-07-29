badabbb08b959ae18b4d316d4bc9ce8f888b0fd1
ybchoi5262 <ybchoi5262@ewhain.net>
2023-07-29 13:58:23 +0900

    🎨 : Btn, InfoContent 수정

--- src/pages/AnswerPage.jsx
+++ src/pages/AnswerPage.jsx
@@ -1,26 +1,71 @@
 import React from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/quizpage/Header';
 import Btn from '../components/_common/Btn';
 
 const AnswerPage = ({ answer, answerInfo }) => {
+    const navigate = useNavigate();
+    const { dDay } = useParams();
+
     return (
         <>
-            <Header></Header>
+            <Header />
             <AnswerWrapper>
                 <CheckAnswer>정답확인 :</CheckAnswer>
-                <Answer>{answer}안녕</Answer>
+                {dDay === '1' ? (
+                    <Answer>{answer}안녕</Answer>
+                ) : dDay === '2' ? (
+                    <Answer>{answer}d2</Answer>
+                ) : dDay === '3' ? (
+                    <Answer>{answer}d3</Answer>
+                ) : dDay === '4' ? (
+                    <Answer>{answer}d4</Answer>
+                ) : dDay === '5' ? (
+                    <Answer>{answer}d5</Answer>
+                ) : dDay === '6' ? (
+                    <Answer>{answer}d6</Answer>
+                ) : dDay === '7' ? (
+                    <Answer>{answer}d7</Answer>
+                ) : null}
             </AnswerWrapper>
-            <AnswerInfo>
-                {answerInfo}무인복합기는 모든 건물에 있지만 흑백은 약 50원,
-                컬러는 약 300~400원 정도의 가격을 지불해야 해요. 용지만 들고
-                간다면 무료로 인쇄가 가능한 곳은 학생문화관 최대 네줄 우하하학
-            </AnswerInfo>
+            {dDay === '1' ? (
+                <AnswerInfo>
+                    {answerInfo}무인복합기는 모든 건물에 있지만 흑백은 약 50원,
+                    컬러는 약 300~400원 정도의 가격을 지불해야 해요. 용지만 들고
+                    간다면 무료로 인쇄가 가능한 곳은 학생문화관 최대 네줄
+                    우하하학
+                </AnswerInfo>
+            ) : dDay === '2' ? (
+                <AnswerInfo>{answerInfo}d2</AnswerInfo>
+            ) : dDay === '3' ? (
+                <AnswerInfo>{answerInfo}d3</AnswerInfo>
+            ) : dDay === '4' ? (
+                <AnswerInfo>{answerInfo}d4</AnswerInfo>
+            ) : dDay === '5' ? (
+                <AnswerInfo>{answerInfo}d5</AnswerInfo>
+            ) : dDay === '6' ? (
+                <AnswerInfo>{answerInfo}d6</AnswerInfo>
+            ) : dDay === '7' ? (
+                <AnswerInfo>{answerInfo}d7</AnswerInfo>
+            ) : null}
+
             <BtnWrapper>
                 <TopBtn>
-                    <Btn text='추가정보 보러가기'></Btn>
+                    <Btn
+                        text='추가정보 보러가기'
+                        onClick={() => {
+                            navigate(`/info/${dDay}`);
+                        }}
+                    />
                 </TopBtn>
-                <Btn text='메인페이지로 돌아가기' type='deepGreen'></Btn>
+                <Btn
+                    text='메인페이지로 돌아가기'
+                    type='deepGreen'
+                    onClick={() => {
+                        navigate(`/`);
+                    }}
+                />
             </BtnWrapper>
         </>
     );
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import Header from '../components/quizpage/Header';
 import Option from '../components/quizpage/Option';
 import Btn from '../components/_common/Btn';
@@ -6,35 +7,46 @@ import styled from 'styled-components';
 
 const QuizPage = () => {
     const [isCorrect, setIsCorrect] = useState(true);
+    const { dDay } = useParams();
+    const navigate = useNavigate();
 
     const handleCheckAnswer = () => {
-        setIsCorrect(isCorrect);
+        const userAnswer = '3';
+        const correctAnswer = '4';
+
+        setIsCorrect(userAnswer === correctAnswer);
+
+        if (userAnswer === correctAnswer) {
+            //modal 띄우기
+            navigate(`/answer/${dDay}`);
+        }
     };
 
     return (
         <>
-            <Header></Header>
-            <Option option1='안녕' option2='어쩔' option3='아니'></Option>
+            <Header />
+            {dDay === '1' ? (
+                <Option option1='안녕' option2='어쩔' option3='아니' />
+            ) : dDay === '2' ? (
+                <Option option1='노노' option2='d2' option3='d2' />
+            ) : dDay === '3' ? (
+                <Option option1='노노' option2='d3' option3='d3_' />
+            ) : dDay === '4' ? (
+                <Option option1='노노' option2='d4' option3='d4_' />
+            ) : dDay === '5' ? (
+                <Option option1='노노' option2='d5' option3='d5_' />
+            ) : dDay === '6' ? (
+                <Option option1='노노' option2='d6' option3='d6_' />
+            ) : dDay === '7' ? (
+                <Option option1='노노' option2='d7' option3='d7_' />
+            ) : null}
             <Wrapper>
-                {isCorrect ? (
-                    <>
-                        <Retry></Retry>
-                        <Btn
-                            type='deepGreen'
-                            text='정답 확인하기'
-                            onClick={handleCheckAnswer}
-                        ></Btn>
-                    </>
-                ) : (
-                    <>
-                        <Retry>다시 생각해보세요!</Retry>
-                        <Btn
-                            type='deepGreen'
-                            text='정답 확인하기'
-                            onClick={handleCheckAnswer}
-                        ></Btn>
-                    </>
-                )}
+                {isCorrect ? <Retry /> : <Retry>다시 생각해보세요!</Retry>}
+                <Btn
+                    type='deepGreen'
+                    text='정답 확인하기'
+                    onClick={handleCheckAnswer}
+                />
             </Wrapper>
         </>
     );
