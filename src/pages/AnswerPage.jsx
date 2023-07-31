d8fb9fac777ee3f5f9710e355daa646a4a618254
ybchoi5262 <ybchoi5262@ewhain.net>
2023-07-31 12:53:28 +0900

    ✨ : AnswerPage 구현 중

--- src/pages/AnswerPage.jsx
+++ src/pages/AnswerPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/quizpage/Header';
 import Btn from '../components/_common/Btn';
@@ -8,10 +8,35 @@ const AnswerPage = ({ answer, answerInfo }) => {
     const navigate = useNavigate();
     const { dDay } = useParams();
 
+    //퀴즈 줄 개수(dDay)에 따라 정답 버튼과 퀴즈 사이의 간격 &
+    //설명 박스와 '추가정보 보러가기'버튼 사이의 간격 다르게 하기
+    let answerMargin;
+    let btnMargin;
+    //'정답확인'(14px)과  정답 버튼을 감싼 wrapper를 기준으로 한 margin-top &
+    // '추가정보 보러가기'버튼 과 설명 박스를 기준으로 한 margin-top
+    switch (dDay) {
+        case '1': //한줄 일 때
+            answerMargin = '92px';
+            btnMargin = '52px';
+            break;
+        case '2': //두줄 일 때
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+            answerMargin = '116px';
+            btnMargin = '51px';
+            break;
+        case '7': //세줄 일 때
+            answerMargin = '115px';
+            btnMargin = '26px';
+            break;
+    }
+
     return (
         <>
             <Header />
-            <AnswerWrapper>
+            <AnswerWrapper style={{ marginTop: answerMargin }}>
                 <CheckAnswer>정답확인 :</CheckAnswer>
                 {dDay === '1' ? (
                     <Answer>{answer}안녕</Answer>
@@ -50,10 +75,11 @@ const AnswerPage = ({ answer, answerInfo }) => {
                 <AnswerInfo>{answerInfo}d7</AnswerInfo>
             ) : null}
 
-            <BtnWrapper>
+            <BtnWrapper style={{ marginTop: btnMargin }}>
                 <TopBtn>
                     <Btn
                         text='추가정보 보러가기'
+                        type='deepGreen'
                         onClick={() => {
                             navigate(`/info/${dDay}`);
                         }}
@@ -61,7 +87,6 @@ const AnswerPage = ({ answer, answerInfo }) => {
                 </TopBtn>
                 <Btn
                     text='메인페이지로 돌아가기'
-                    type='deepGreen'
                     onClick={() => {
                         navigate(`/`);
                     }}
@@ -92,7 +117,10 @@ const Answer = styled.div`
     color: var(--white);
 `;
 
-const CheckAnswer = styled.div``;
+const CheckAnswer = styled.div`
+    font-size: 12px;
+    font-weight: 500;
+`;
 
 const AnswerInfo = styled.div`
     background-color: var(--gray1);
@@ -115,11 +143,6 @@ const BtnWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
-    position: absolute;
-    bottom: 66px;
-    left: 0;
-    right: 0;
 `;
 
 const TopBtn = styled.div`
